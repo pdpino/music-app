b8225c9eba0513b985e6c94d5a45e068f074e719
Pablo Pino <pdpino@uc.cl>
2017-09-24 17:05:07 -0300

    Add authorization requirement for editing an artist (only creator can edit)

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,7 @@
 class ArtistsController < ApplicationController
   before_action :set_artist, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @artists = Artist.all
@@ -44,8 +45,18 @@ class ArtistsController < ApplicationController
       @artist = Artist.find(params[:id])
     end
 
+    def set_user
+      @user = User.find(@artist.user_id)
+    end
+
     def artist_params
       params.require(:artist).permit(:name, :description, :country, :members, :active_since, :active_until)
+    end
+
+    def correct_user
+      # set_artist # Assume this is already called, discomment if necessary
+      set_user
+      redirect_to root_path unless current_user?(@user)
     end
 
 end
