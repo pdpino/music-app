8209ac74f78c86e9ba3a6af15480f86b81a35d69
Pablo Pino <pdpino@uc.cl>
2017-09-24 01:06:21 -0300

    Create basic home page
    Add a basic home page
    Add require_user when seeing artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,6 @@
 class ArtistsController < ApplicationController
   before_action :set_artist, only: [:show, :edit, :update, :destroy]
+  before_action :require_user #,  only: [:index, :show, :new, ]
 
   def index
     @artists = Artist.all
