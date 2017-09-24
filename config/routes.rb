35be09f511899f6cb99612bbca528ecbfde0e2d6
Pablo Pino <pdpino@uc.cl>
2017-09-24 00:15:16 -0300

    Add signup page
    Add user model
    Add user controller for new and create actions
    Add basic signup page, still missing validations on input
    missing session login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'signup'  => 'users#new'
+  resources :users
+
   resources :artists
   ## resources is equivalent to:
   # get 'artists' => 'artists#index'
