9d1407353b52667eb26000a2396fa1a91c195d8c
Pablo Pino <pdpino@uc.cl>
2017-09-24 00:45:29 -0300

    Add login
    Add session/new controller
    Add session/create controller (POST and create the session)
    Add delete session controller (logout)
    Add routes for all the above
    Add methods in ApplicationController to manage current_user and to require_user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'signup'  => 'users#new'
   resources :users
 
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   resources :artists
   ## resources is equivalent to:
   # get 'artists' => 'artists#index'
