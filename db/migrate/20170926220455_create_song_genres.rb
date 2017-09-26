67001977f0a5ffd359baed61a73f48d9d6479430
Pablo Pino <pdpino@uc.cl>
2017-09-26 19:13:51 -0300

    Add foreign keys to SongGenres migration

--- db/migrate/20170926220455_create_song_genres.rb
+++ db/migrate/20170926220455_create_song_genres.rb
@@ -1,6 +1,8 @@
 class CreateSongGenres < ActiveRecord::Migration[5.0]
   def change
     create_table :song_genres do |t|
+      t.belongs_to :song, index: true
+      t.belongs_to :genre, index: true
 
       t.timestamps
     end
