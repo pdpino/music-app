541477d5e5d2e246e899618a3c314c4dcda96271
Pablo Pino <pdpino@uc.cl>
2017-09-26 19:34:45 -0300

    In Song Controller, move before_actions about user to the top

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,10 @@
 class SongsController < ApplicationController
+  before_action :require_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :set_song, only: [:show, :edit, :update, :destroy]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :set_all_attributes, only: [:new, :create, :edit, :update]
   before_action :set_song_attributes, only: [:show, :edit, :update]
-  before_action :require_user, only: [:new, :create, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @has_create_permission = current_user || false
