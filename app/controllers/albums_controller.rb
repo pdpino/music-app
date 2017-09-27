557cdd80b1b910002c7dc9b0d01e061d8b4b11d4
Pablo Pino <pdpino@uc.cl>
2017-09-26 22:38:24 -0300

    Add view of AlbumArtists from album side

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,8 +5,8 @@ class AlbumsController < ApplicationController
   before_action :require_user, only: [:new, :create, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
 
-  # before_action :set_all_entities, only: [:new, :create, :edit, :update]
-  # before_action :set_song_attributes, only: [:show, :edit, :update]
+  before_action :set_all_entities, only: [:new, :create, :edit, :update]
+  before_action :set_album_attributes, only: [:show, :edit, :update]
 
   def index
     @has_create_permission = current_user || false
@@ -23,8 +23,8 @@ class AlbumsController < ApplicationController
 
     # REVIEW: neccesary ??
     # in _form this things are used,
-    # @song_artists = Array.new # Empty array
-    # @song_genres = Array.new # Empty array
+    @album_artists = Array.new # Empty array
+    @album_songs = Array.new # Empty array
   end
 
   def edit
@@ -61,32 +61,25 @@ class AlbumsController < ApplicationController
       permitted = params.require(:album).permit(:name, :description, :release_date, :is_single, :artwork_img_name)
 
       # OPTIMIZE
-      # params[:all_artists] ||= Array.new
-      # selected_artists_id = params[:all_artists].map do |x| x.to_i end
-      # permitted[:artists] = Artist.find(selected_artists_id)
-      #
-      # params[:all_genres] ||= Array.new
-      # selected_genres_id = params[:all_genres].map do |x| x.to_i end
-      # permitted[:genres] = Genre.find(selected_genres_id)
+      params[:all_artists] ||= Array.new
+      selected_artists_id = params[:all_artists].map do |x| x.to_i end
+      permitted[:artists] = Artist.find(selected_artists_id)
 
       permitted
-      # NOTE: do not permit :owner_id, an user would be able to fake the song creator
     end
 
     def set_album
       @album = Album.find(params[:id])
     end
 
-    # def set_all_entities
-    #   # NOTE: parse to array to be able to match intersection with @song_artists
-    #   @all_artists = Array.new Artist.all
-    #   @all_genres = Array.new Genre.all
-    # end
-    #
-    # def set_song_attributes
-    #   @song_artists = Array.new @song.artists
-    #   @song_genres = Array.new @song.genres
-    # end
+    def set_all_entities
+      @all_artists = Array.new Artist.all
+    end
+
+    def set_album_attributes
+      @album_artists = Array.new @album.artists
+      @album_songs = Array.new @album.songs
+    end
 
     def set_user
       @user = User.find(@album.owner_id)
