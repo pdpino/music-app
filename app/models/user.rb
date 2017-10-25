de18ef91c80d0135257f4a3c6b45d1a6a776c951
Pablo Pino <pdpino@uc.cl>
2017-10-25 16:42:57 -0300

    Add the validation of a stronger password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
+# source: https://stackoverflow.com/questions/5123972/ruby-on-rails-password-validation
+PASSWORD_FORMAT = /\A
+  (?=.*\d)           # Must contain a digit
+  (?=.*[a-z])        # Must contain a lower case character
+  (?=.*[A-Z])        # Must contain an upper case character
+/x
+
 class User < ApplicationRecord
   has_many :artists, foreign_key: :owner_id
   has_many :songs, foreign_key: :owner_id
@@ -6,7 +13,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 8 }, format: { with: PASSWORD_FORMAT }
 
-  validates_confirmation_of :password
+  # validates_confirmation_of :password
 end
