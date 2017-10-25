0b2fc7f9aced1bc7a33dc6fbf1282e569b6249fe
Pablo Pino <pdpino@uc.cl>
2017-10-25 17:25:52 -0300

    Add show error messages in edit user view

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true, length: { minimum: 8 }, format: { with: PASSWORD_FORMAT }
 
-  # validates_confirmation_of :password
 end
