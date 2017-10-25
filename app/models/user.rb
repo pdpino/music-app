f466ddd2bc250769d7f05f89a52b6ab62c569004
Pablo Pino <pdpino@uc.cl>
2017-10-25 20:36:24 -0300

    Edit user form to include or not password

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,11 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, confirmation: true, length: { minimum: 8 }, format: { with: PASSWORD_FORMAT }
+  validates :password, presence: { if: :password_required?, message: 'is forgotten.' }, confirmation: true, length: { minimum: 8 }, format: { with: PASSWORD_FORMAT }
+
+  def password_required?
+    # FIXME: true when updating password and creating, false when updating info
+    false
+  end
 
 end
