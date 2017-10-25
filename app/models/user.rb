079942c9a5cdbb2150bc284d851ff4c5f62fe4d5
Pablo Pino <pdpino@uc.cl>
2017-10-25 16:35:08 -0300

    Add confirm password handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   has_many :artists, foreign_key: :owner_id
   has_many :songs, foreign_key: :owner_id
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+
+  validates_confirmation_of :password
 end
