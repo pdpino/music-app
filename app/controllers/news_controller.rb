b1e5d251223a809d112c8003a435452aba8b969e
Pablo Pino <pdpino@uc.cl>
2017-11-26 18:37:24 -0300

    Add relations between news and artists/albums/songs
    Can be added and removed from a news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,6 +2,8 @@ class NewsController < ApplicationController
   before_action :require_admin, only: [:new, :create, :edit, :update, :delete]
   before_action :set_news, only: [:show, :edit, :update, :destroy]
   before_action :set_has_permission, only: [:show, :index]
+  before_action :set_all_entities, only: [:new, :create, :edit, :update]
+  before_action :set_news_attributes, only: [:edit, :update]
 
   def index
     # REFACTOR: this is copied in all the controllers with search
@@ -17,6 +19,10 @@ class NewsController < ApplicationController
 
   def new
     @news = News.new
+
+    @news_artists = Array.new
+    @news_albums = Array.new
+    @news_songs = Array.new
   end
 
   def create
@@ -45,33 +51,24 @@ class NewsController < ApplicationController
     redirect_to news_index_path
   end
 
-  # def add_song_news
-  #   add_news(@song)
-  # end
-  #
-  # def remove_song_news
-  #   remove_news(@song)
-  # end
-  #
-  # def add_artist_news
-  #   add_news(@artist)
-  # end
-  #
-  # def remove_artist_news
-  #   remove_news(@artist)
-  # end
-  #
-  # def add_album_news
-  #   add_news(@album)
-  # end
-  #
-  # def remove_album_news
-  #   remove_news(@album)
-  # end
-
   private
     def news_params
-      @params = params.require(:news).permit(:title, :content, :image)
+      permitted = params.require(:news).permit(:title, :content, :image)
+
+      # OPTIMIZE
+      params[:all_artists] ||= Array.new
+      selected_artists_id = params[:all_artists].map do |x| x.to_i end
+      permitted[:artists_news] = Artist.find(selected_artists_id)
+
+      params[:all_songs] ||= Array.new
+      selected_songs_id = params[:all_songs].map do |x| x.to_i end
+      permitted[:songs_news] = Song.find(selected_songs_id)
+
+      params[:eligible_albums] ||= Array.new
+      selected_albums_id = params[:eligible_albums].map do |x| x.to_i end
+      permitted[:albums_news] = Album.find(selected_albums_id)
+
+      permitted
     end
 
     def set_news
@@ -81,4 +78,18 @@ class NewsController < ApplicationController
     def set_has_permission
       @has_permission = is_current_user_admin?
     end
+
+    def set_all_entities
+      # NOTE: parse to array to be able to match intersection with @song_artists
+      @all_artists = Array.new Artist.all
+      @all_songs = Array.new Song.all
+      @eligible_albums = Array.new Album.all
+    end
+
+    def set_news_attributes
+      @news_artists = Array.new @news.artists_news
+      @news_albums = Array.new @news.albums_news
+      @news_songs = Array.new @news.songs_news
+    end
+
 end
