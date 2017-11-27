10f44e08a9f63b293983028903cc33438f8d10bd
Pablo Pino <pdpino@uc.cl>
2017-11-26 22:08:12 -0300

    Minor fix in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+## Descriptions taken from wikipedia
+
 require 'faker'
 
 admin = User.create(
